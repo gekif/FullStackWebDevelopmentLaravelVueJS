68b65e8c37392d1ebe2635bc488b469055b5f7f0
Gekif <dzulfikar.maulana@gmail.com>
2019-07-18 21:58:26 +0700

    4. Designing Question Schema - Part 2 of 2

--- laravel-qa/app/Question.php
+++ laravel-qa/app/Question.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $fillable = ['title', 'body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * $question = Question::find(1);
+     * $question->user->email
+     */
 }
