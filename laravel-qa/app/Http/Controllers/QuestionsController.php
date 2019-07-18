0526c68753491c04c292a3543f618b3b9bf6f64a
Gekif <dzulfikar.maulana@gmail.com>
2019-07-19 06:06:47 +0700

    9. Adding Author info and Question creation date on Question item - Part 2 of 2

--- laravel-qa/app/Http/Controllers/QuestionsController.php
+++ laravel-qa/app/Http/Controllers/QuestionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class QuestionsController extends Controller
 {
@@ -14,9 +15,14 @@ class QuestionsController extends Controller
      */
     public function index()
     {
-        $questions = Question::latest()->paginate(5);
+//        \DB::enableQueryLog();
+
+        $questions = Question::with('user')->latest()->paginate(10);
 
         return view('questions.index', compact('questions'));
+//            ->render();
+
+//        dd(\DB::getQueryLog());
     }
 
 
