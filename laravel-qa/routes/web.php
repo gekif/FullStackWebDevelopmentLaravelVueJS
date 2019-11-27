1d0e3057ff814f4caf27346d76539289f2f9e47d
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-27 14:48:17 +0700

    Display answers of question

--- laravel-qa/routes/web.php
+++ laravel-qa/routes/web.php
@@ -20,4 +20,9 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('questions', 'QuestionsController')->except('show');
+
+//Route::post('/questions/{questions}/answers', 'AnswersController@store')->name('anwsers.store');
+
+Route::resource('questions.answers', 'AnswersController')->except(['index', 'create', 'show']);
+
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
