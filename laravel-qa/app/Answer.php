e0ad70f6fd94ead681304f866d02405da5dd69a7
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-28 10:25:00 +0700

    Implement delete functionality for answer

--- laravel-qa/app/Answer.php
+++ laravel-qa/app/Answer.php
@@ -7,21 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Answer extends Model
 {
     protected $fillable = ['body', 'user_id'];
+
     
     public function question()
     {
         return $this->belongsTo(Question::class);
     }
 
+
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+
     public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
     }
+
     
     public static function boot()
     {
@@ -29,9 +33,13 @@ class Answer extends Model
 
         static::created(function ($answer) {
             $answer->question->increment('answers_count');
-            $answer->question->save();            
-        });        
+        });
+
+        static::deleted(function ($answer) {
+            $answer->question->decrement('answers_count');
+        });
     }
+
 
     public function getCreatedDateAttribute()
     {
