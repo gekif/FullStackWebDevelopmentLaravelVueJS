e28aeb0ec3879a4ad68893c483de37d392a6380d
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-04 15:48:33 +0700

    Implement accept the answer as best answer

--- laravel-qa/app/Question.php
+++ laravel-qa/app/Question.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     protected $fillable = ['title', 'body'];
+
     
     public function user() {
         return $this->belongsTo(User::class);
-    }    
+    }
+
 
     public function setTitleAttribute($value)
     {
@@ -18,15 +20,18 @@ class Question extends Model
         $this->attributes['slug'] = str_slug($value);
     }
 
+
     public function getUrlAttribute()
     {
         return route("questions.show", $this->slug);
     }
 
+
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
     }
+
 
     public function getStatusAttribute()
     {
@@ -39,15 +44,25 @@ class Question extends Model
         return "unanswered";
     }
 
+
     public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
     }
+
 
     public function answers()
     {
         return $this->hasMany(Answer::class);
         // $question->answers->count()
         // foreach ($question->answers as $answer)
+    }
+
+
+    public function acceptBestAnswer(Answer $answer)
+    {
+        $this->best_answer_id = $answer->id;
+
+        $this->save();
     }
 }
