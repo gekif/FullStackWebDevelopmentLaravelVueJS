1f01e3422589e5a6c4bfd35f850ec9c8db5e35a7
Gekif <dzulfikar.maulana@gmail.com>
2020-03-15 10:21:06 +0700

    Create a basic Vue.js component

--- laravel-qa-vue/app/Answer.php
+++ laravel-qa-vue/app/Answer.php
@@ -9,7 +9,9 @@ class Answer extends Model
     use VotableTrait;
     
     protected $fillable = ['body', 'user_id'];
-    
+
+    protected $appends = ['created_date'];
+
     public function question()
     {
         return $this->belongsTo(Question::class);
