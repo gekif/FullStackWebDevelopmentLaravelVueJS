9568f208ec7c2575777b5914686f39a40b88633b
Gekif <dzulfikar.maulana@gmail.com>
2020-03-18 21:47:52 +0700

    Ajaxify the delete button

--- laravel-qa-vue/public/js/app.js
+++ laravel-qa-vue/public/js/app.js
@@ -49721,7 +49721,7 @@ exports = module.exports = __webpack_require__(11)(false);
 
 
 // module
-exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
+exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
 
 // exports
 
@@ -49766,7 +49766,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
         update: function update() {
             var _this = this;
 
-            axios.patch("/questions/" + this.questionId + "/answers/" + this.id, {
+            axios.patch(this.endpoint, {
                 body: this.body
             }).then(function (res) {
                 _this.editing = false;
@@ -49774,12 +49774,26 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             }).catch(function (err) {
                 console.log(err.response.data.message);
             });
+        },
+        destroy: function destroy() {
+            var _this2 = this;
+
+            if (confirm('Are you sure?')) {
+                axios.delete(this.endpoint).then(function (res) {
+                    $(_this2.$el).fadeOut(500, function () {
+                        alert(res.data.message);
+                    });
+                });
+            }
         }
     },
 
     computed: {
         isInvalid: function isInvalid() {
             return this.body.length < 10;
+        },
+        endpoint: function endpoint() {
+            return '/questions/' + this.questionId + '/answers/' + this.id;
         }
     }
 });
