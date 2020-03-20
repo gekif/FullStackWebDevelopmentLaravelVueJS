80d9a953f4a2024647cd52c5a44ffcacae13a62b
Gekif <dzulfikar.maulana@gmail.com>
2020-03-20 13:22:58 +0700

    Rewrite the favorite button into Vue.js component

--- laravel-qa-vue/resources/views/layouts/app.blade.php
+++ laravel-qa-vue/resources/views/layouts/app.blade.php
@@ -10,8 +10,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -71,5 +69,15 @@
             @yield('content')
         </main>
     </div>
+
+    <!-- Scripts -->
+    <script>
+        window.Auth = {!! json_encode([
+            'signedIn' => Auth::check(),
+            'user' => Auth::user()
+        ]) !!}
+    </script>
+
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- laravel-qa-vue/resources/views/shared/_vote.blade.php
+++ laravel-qa-vue/resources/views/shared/_vote.blade.php
@@ -38,11 +38,11 @@
         @csrf
         <input type="hidden" name="vote" value="-1">
     </form>
-    
+
+
     @if ($model instanceof App\Question)
-        @include ('shared._favorite', [
-            'model' => $model
-        ])
+        <favorite :question="{{ $model }}"></favorite>
+
     @elseif ($model instanceof App\Answer)
         @include ('shared._accept', [
             'model' => $model
